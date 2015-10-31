f838dc4bd587e1b92a9ec9f2fe634496c247d0c3
Antonino Parisi <tabman83@gmail.com>
2015-10-31 15:29:30 +0000

    Selection of items.

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -4,36 +4,39 @@ angular.module('NetPlanningApp').controller('AppCtrl', function($mdSidenav) {
 		$mdSidenav(menuId).toggle();
 	};
 
-	// 0 lesson, 1 available
+	// 0 available
+	// 1 available by me
+	// 2 recurrent lesson
+	// 3 1-time lesson
     this.items = [{
         name: 'Jubon Aurelie',
-		type: 0,
+		type: 2,
         date: new Date(2015, 10, 2, 10, 30)
   	}, {
-		type: 1,
+		type: 0,
         date: new Date(2015, 10, 2, 11, 00)
   	}, {
         name: 'Castro Maurice',
-		type: 0,
+		type: 2,
         date: new Date(2015, 10, 2, 11, 30)
   	}, {
         name: 'Lijion Miriam',
-		type: 0,
+		type: 3,
         date: new Date(2015, 10, 2, 12, 00)
   	}, {
-		type: 1,
+		type: 0,
         date: new Date(2015, 10, 2, 12, 30)
   	}, {
         name: 'Oboez Marc',
-		type: 0,
+		type: 3,
         date: new Date(2015, 10, 2, 13, 00)
   	}, {
         name: 'Ducette Ivette',
-		type: 0,
+		type: 2,
         date: new Date(2015, 10, 2, 13, 30)
   	}, {
         name: 'Devrais Aloise',
-		type: 0,
+		type: 3,
         date: new Date(2015, 10, 2, 15, 00)
   	}, {
 		type: 1,
