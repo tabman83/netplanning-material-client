840ea9352cbd4474a132d742f30c9a3dfc00fa6c
Antonino Parisi <tabman83@gmail.com>
2015-11-04 22:48:17 +0000

    Italian localization.

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,7 +1,9 @@
-angular.module('NetPlanningApp').controller('SettingsCtrl', function() {
+angular.module('NetPlanningApp').controller('SettingsCtrl', function($localStorage) {
 
 	var vm = this;
 
+	vm.$localStorage = $localStorage;
+	
 	vm.availableLanguages = {
 		'en': 'English',
 		'it': 'Italiano'
