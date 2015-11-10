271717a5d82c8d298f1515bcab160c1cb726f5d1
tabman83 <tabman83@gmail.com>
2015-11-10 16:02:46 +0000

    Watch templates and partials.

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,7 @@ gulp.task('serve', ['styles', 'fonts'], function () {
 
   // watch for changes
   gulp.watch([
-    'app/*.html',
+    'app/**/*.html',
     'app/scripts/**/*.js',
     'app/images/**/*',
     '.tmp/fonts/**/*'
